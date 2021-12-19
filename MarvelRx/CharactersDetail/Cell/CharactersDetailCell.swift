9cfd3101519e53ebca0fb640701982e3096ef1d8
hamza jerbi <hamzon.jb@gmail.com>
2021-12-19 23:40:11 +0100

    binded  comics v1

---
+++ MarvelRx/CharactersDetail/Cell/CharactersDetailCell.swift
@@ -0,0 +1,36 @@
+//
+//  CharactersDetailCell.swift
+//  MarvelRx
+//
+//  Created by Hamza on 19/12/21.
+//
+
+import Foundation
+import UIKit
+import Kingfisher
+
+class CharactersDetailCell: UICollectionViewCell {
+    
+    @IBOutlet weak var lblSerieName: UILabel!
+    
+    static var cellType: String {
+        return String(describing: self)
+    }
+
+    // MARK: - Properties
+    var viewModel: thumbnailComicsItem! {
+        didSet {
+            self.configure()
+        }
+    }
+}
+
+// MARK: - Configuration
+extension CharactersDetailCell {
+    private func configure() {
+        
+        
+        lblSerieName.text = viewModel.name
+
+    }
+}
--- MarvelRx/CharactersDetail/View/CharactersDetailViewController.swift
+++ MarvelRx/CharactersDetail/View/CharactersDetailViewController.swift
@@ -11,22 +11,45 @@ import RxSwift
 import RxCocoa
 import Kingfisher
 
-class CharactersDetailViewController: UIViewController {
+class CharactersDetailViewController: UIViewController,UICollectionViewDelegateFlowLayout {
     
     @IBOutlet weak var lblDesc: UILabel!
     @IBOutlet weak var imgHero: UIImageView!
     @IBOutlet weak var lblHeroName: UILabel!
+    @IBOutlet weak var characterCv: UICollectionView!
     // MARK: - Lifecycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
+        registerCollection()
         bindViewModel()
-
+        bindCollectionView()
+        
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         viewModel.didClose.onNext(())
     }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 5.0, left: 1.0, bottom: 1.0, right: 1.0)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let lay = collectionViewLayout as! UICollectionViewFlowLayout
+      
+        let widthPerItem = collectionView.frame.width / 2 - lay.minimumInteritemSpacing
+
+        return CGSize(width:widthPerItem, height:200)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout
+        collectionViewLayout: UICollectionViewLayout,
+                        minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 2
+    }
+    
 
     // MARK: - Properties
     let disposeBag = DisposeBag()
@@ -36,12 +59,32 @@ class CharactersDetailViewController: UIViewController {
 
 // MARK: - Binding
 extension CharactersDetailViewController {
-  
+    
     func bindViewModel() {
         self.lblHeroName.text = viewModel.name
         self.lblDesc.text = viewModel.description
         let url =  URL(string: viewModel.thumbnail.fullName)
         imgHero.kf.setImage(with: url)
+    }
+    
+    func registerCollection(){
+        
+        characterCv.register(UINib(nibName: "CharactersDetailCell", bundle: nil), forCellWithReuseIdentifier: "CharactersDetailCell")
+        characterCv.rx.setDelegate(self).disposed(by: disposeBag)
+    }
+    
+    
+    
+    func bindCollectionView(){
+        
+        let arrayComics = (viewModel.comics.items)!
+        let comics = BehaviorSubject<[thumbnailComicsItem]>(
+            value: arrayComics)
+                
+        comics.bind(to: characterCv.rx.items(cellIdentifier: "CharactersDetailCell",cellType: CharactersDetailCell.self)) { index, viewModel, cell in
+            cell.viewModel = viewModel
+                    }.disposed(by: disposeBag)
+
     }
 }
 
